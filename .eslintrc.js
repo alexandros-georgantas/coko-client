49a78f303c16dcfb4280d7059812d83cc9b438d7
Yannis Barlas <yannisbarlas@gmail.com>
2021-01-09 21:54:06 +0200

    feat(*): publish transpiled code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,3 @@
 const { eslint } = require('@coko/lint')
 
-const allowDevImportPaths =
-  eslint.rules['import/no-extraneous-dependencies'][1].devDependencies
-const pattern = 'dev/**'
-if (!allowDevImportPaths.includes(pattern)) allowDevImportPaths.push(pattern)
-
 module.exports = eslint
